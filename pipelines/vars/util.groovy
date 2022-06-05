862b45c7dd020d1429c181a1dcaaa4885531f8d3
Gabriel Oliveira <oliveira992gabriel@gmail.com>
2022-06-05 18:36:35 -0300

    add files devops v1

---
+++ pipelines/vars/util.groovy
@@ -0,0 +1,31 @@
+def call() {
+    echo "Welcome"
+}
+
+
+pipeline {
+    agent {
+        node {
+            label 'ubuntu'
+        }
+    }
+    stages {
+        stage('Clone Repo Project') {
+            steps {
+               util()
+            }
+        }
+        
+    }
+    post {
+        success {
+            echo "THE END JOB SUCCEEDED!"
+        }
+        failure {
+            echo "FALHA !!! "
+        }
+        always {
+            cleanWs deleteDirs: true, patterns: [[pattern: '', type: 'EXCLUDE']]
+        }      
+    }       
+}
